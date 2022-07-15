fe3b9bd8f3c2b767cef45c4507f95f4b52d90772
log5 <40707031+log5r@users.noreply.github.com>
2022-07-15 16:43:51 +0900

    add todo list

--- statistic/switch/screenshotc/count_switch_ss.sh
+++ statistic/switch/screenshotc/count_switch_ss.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+// TODO 自力でパス移動するの面倒なのでなんとかする
+// TODO 日付部分を手作業でトリムするの面倒なのでなんとかする
+// TODO フォルダ名じゃなくてタイムスタンプを参照すべきであろう
+
 for d in `find . -type d`;
     do echo $d,`ls -1UR "$d" | wc -l`; 
 done;
